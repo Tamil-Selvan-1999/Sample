4a0384100c36bc0b2bd43a2de7a14eb17616485a
Tamil-Selvan-1999 <141508207+Tamil-Selvan-1999@users.noreply.github.com>
2023-10-11 07:13:08 +0530

    Add files via upload

---
+++ EnablerService.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * @file
+ * Contains the RSVP Enabler service
+ */
+
+namespace Drupal\rsvplist;
+
+use Drupal\Core\Database\Connection;
+use Drupal\node\Entity\Node;
+
+class EnablerService {
+
+    protected $database_connection;
+
+    public function __construct(Connection $connection)
+    {
+        $this->database_connection = $connection;
+    }
+
+    public function isEnabled(Node &$node) {
+        if($node->isNew()) {
+            return FALSE;
+        }
+        try {
+            $select = $this->database_connection->select('rsvplist_enabled', 're');
+            $select->fields('re', ['nid']);
+            $select->condition('nid', $node->id());
+            $result = $select->execute();
+
+            return !(empty($result->fetchCol()));
+        } catch (\Exception $e) {
+            \Drupal::messenger()->addError(t('Unable to determine RSVP settings st this time. Please try again.'));
+            return NULL;
+        }
+    }
+
+    /**
+     * Sets an individual node to the RSVP enabled.
+     * 
+     * @param Node $node
+     * @throws Exception
+     */
+    public function setEnabled(Node $node) {
+        try {
+            if (! ($this->isEnabled($node))) {
+                $insert = $this->database_connection->insert('rsvplist_enabled');
+                $insert->fields(['nid']);
+                $insert->values([$node->id()]);
+                $insert->execute();
+            }
+        } catch (\Exception $e) {
+            \Drupal::messenger()->addError(t('Unable to save RSVP settings at this time. Please try again'));
+        }
+    }
+
+    /**
+     * Deletes RSVP enabled settings for an individual node.
+     * 
+     * @param Node $node
+     */
+    public function delEnabled(Node $node) {
+        try {
+            $delete = $this->database_connection->delete('rsvplist_enabled');
+            $delete->condition('nid', $node->id());
+            $delete->execute();
+        } catch (\Exception $e) {
+            \Drupal::messenger()->addError(t('Unable to sae RSVP settings at this time. Please try again.'));
+        }
+    }
+}
